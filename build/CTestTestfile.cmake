bd7ebfb13614b7076e9d0ddc290ef9403c9f75e1
Maxence GIBARU <maxence.gibaru@student.junia.com>
2024-03-24 21:30:00 +0100

    update remove build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maxence/Desktop/workspace/github/junia/algo-tp/CIR2-ALGO-TP
-# Build directory: /home/maxence/Desktop/workspace/github/junia/algo-tp/CIR2-ALGO-TP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
